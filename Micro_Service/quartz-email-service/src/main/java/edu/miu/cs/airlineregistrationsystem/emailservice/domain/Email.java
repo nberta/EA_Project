fbcf0a776c0ee426374a877e79197bed5610e4c8
Nina <thilenguyen.dao@gmail.com>
2020-06-15 22:54:43 -0500

    delete old email service

--- Micro_Service/quartz-email-service/src/main/java/edu/miu/cs/airlineregistrationsystem/emailservice/domain/Email.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.cs.airlineregistrationsystem.emailservice.domain;
-
-public abstract class Email {
-
-    public abstract String getRecipientAddress();
-    public abstract String getSubject();
-    public abstract String getBody();
-}
