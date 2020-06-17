847a177ea94416663d2db1dd009ff0220ee72c1e
nahom <46569017+kafkasdog4@users.noreply.github.com>
2020-06-17 09:36:34 -0500

    minor passenger-aggregator edits

--- Micro_Service/cs544_project_auth_service/src/main/java/edu/miu/cs544/aggregator/service/PassengerService.java
+++ Micro_Service/cs544_project_auth_service/src/main/java/edu/miu/cs544/aggregator/service/PassengerService.java
@@ -3,8 +3,12 @@ package edu.miu.cs544.aggregator.service;
 import java.util.List;
 
 import edu.miu.cs544.service.aggregator.response.PassengerResponse;
+import edu.miu.cs544.service.request.PassengerRequest;
 
 public interface PassengerService {
 	PassengerResponse getById(Integer id);
 	List<PassengerResponse> getAll();
+	PassengerResponse save(PassengerRequest passengerRequest);
+	PassengerResponse putPassenger(PassengerRequest passengerRequest, Integer id);
+	PassengerResponse deleteById(Integer id);
 }
